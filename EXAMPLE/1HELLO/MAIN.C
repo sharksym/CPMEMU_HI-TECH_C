ff0981512d473edaf10f525dda7fceb7faa3f7fc
Yeongman Seo <sharksym@hitel.net>
2015-04-21 00:51:46 +0900

    EXAMPLE: 1HELLO: Added demo
    Signed-off-by: Yeongman Seo <sharksym@hitel.net>

---
+++ EXAMPLE/1HELLO/MAIN.C
@@ -0,0 +1,19 @@
+/*
+ *  HI-TECH C demo by Yeongman Seo <sharksym@hitel.net>
+ *
+ *  1HELLO
+ *
+ *  app-mode = 1
+ *  bank no. = 0
+ *  lib-float = 1
+ *  lib-r800 = 0
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
